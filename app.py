5e354e264eca3816a84f5211007a392172979106
Andres Haehnel <ahaehnel@kiusys.com>
2022-01-25 18:01:18 -0300

    update reame

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from entities.game_controller import GameController
 
 if __name__ == "__main__":
     GAME = GameController()
-    GAME.add_cpu_player("CPU2")    # game.add_cpu_player("CPU2")
+    GAME.add_human_player("Player1")
+    # game.add_cpu_player("CPU2")
     GAME.add_cpu_player("CPU1")
     GAME.begin_game()
